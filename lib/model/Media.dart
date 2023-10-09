b85ef4c477e67a2c7e92612f4b5d36fa6e45def8
sr <srzzuares@gmail.com>
2023-10-08 23:22:53 -0600

    first commit

---
+++ lib/model/Media.dart
@@ -0,0 +1,43 @@
+// Creado por Crystian Enrique Suarez Cuevas <200527>
+// 06-10-2023
+// Asignatura: Desarrollo Movil Integral
+// Grado: 10° Grupo: A
+// Profesor MTI. Marco Antorio Ramirez Hernandez
+import 'package:dmi_moviedb_practica10_200527_flutter/common/Util.dart';
+
+class Media {
+  int id;
+  double voteAverage;
+  String title;
+  String posterPath;
+  String backdropPath;
+  String overview;
+  String releaseDate;
+  List<dynamic> genreIds;
+
+  String getPosterUrl() => getMediumPictureUrl(posterPath);
+  String getBackDropUrl() => getLargePictureUrl(backdropPath);
+  String getGenres() => getGenreValues(genreIds);
+  int getReleaseYear() {
+    if (releaseDate == null || releaseDate == "") return 0;
+    return DateTime.parse(releaseDate).year;
+  }
+
+  factory Media(Map jsonMap) {
+    try {
+      return new Media.deserealize(jsonMap);
+    } catch (ex) {
+      throw ex;
+    }
+  }
+
+  Media.deserealize(Map json)
+      : id = json["id"].toInt(),
+        voteAverage = json["vote_average"].toDouble(),
+        title = json["title"],
+        posterPath = json["poster_path"] ?? "",
+        backdropPath = json["backdrop_path"] ?? "",
+        overview = json["overview"],
+        releaseDate = json["release_date"],
+        genreIds = json["genre_ids"].toList();
+}
